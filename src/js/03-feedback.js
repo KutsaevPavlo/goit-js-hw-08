deb8c5ce658fab34d7aacefe1c9845526fad5cba
KutsaevPavlo <p1ul@ukr.net>
2023-01-15 22:47:22 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+const localObject = {email:' ', message:' '};
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener("input", (event) => {
+    event.preventDefault();
+    
+    localObject[event.target.name] = event.target.value
+
+    // const localObject = {
+    //   elements: { email, message }
+    // } = event.currentTarget;
+
+    // console.log(email.value, message.value);
+    localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(localObject));
+  });
