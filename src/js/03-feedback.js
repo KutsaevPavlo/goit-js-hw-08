f11dadbfaaeb3c68740a6129cdac0cf1c2c41ab4
Pavlo <p1kutsaev@gmail.com>
2023-01-16 16:30:09 +0200

    update task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,8 @@
 import throttle from 'lodash.throttle';
 const FEEDBACK_FORM_STATE = 'feedback-form-state';
 const localObject = {email:' ', message:' '};
+const emailInput = document.querySelector("input");
+const messageInput = document.querySelector("textarea");
 
 const form = document.querySelector('.feedback-form');
 
@@ -16,3 +18,20 @@ form.addEventListener("input", (event) => {
     // console.log(email.value, message.value);
     localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(localObject));
   });
+
+
+  const dataLocalObject = localStorage.getItem(FEEDBACK_FORM_STATE);
+  function insrtFromLocalStorage(object)
+  {
+    if(object && emailInput.name === "email"){
+      emailInput.value = JSON.parse(object).email;
+    }
+    if(object && messageInput.name === "message"){
+      messageInput.value = JSON.parse(object).message;
+    }
+  }
+  insrtFromLocalStorage(dataLocalObject);
+
+
+
+  
