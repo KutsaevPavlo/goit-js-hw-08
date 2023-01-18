75467e7c8c289a7f59c5bd3cc575a56a7ae13d9d
Pavlo <p1kutsaev@gmail.com>
2023-01-18 12:47:44 +0200

    update task 03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,8 @@
 import throttle from 'lodash.throttle';
 const FEEDBACK_FORM_STATE = 'feedback-form-state';
 let localObject = {email:' ', message:' '};
-const emailInput = document.querySelector("input");
-const messageInput = document.querySelector("textarea");
+// const emailInput = document.querySelector("input");
+// const messageInput = document.querySelector("textarea");
 
 const form = document.querySelector('.feedback-form');
 
@@ -16,34 +16,54 @@ form.addEventListener("input", throttle((event) => {
 
 
   const dataLocalObject = localStorage.getItem(FEEDBACK_FORM_STATE);
-  function insrtFromLocalStorage(object)
+  function insrtFromLocalStorage(dataLocalObject)
   {
-    if(object && emailInput.name === "email"){
-      emailInput.value = JSON.parse(object).email;
+    if (!dataLocalObject) return
+    
+    // console.log(localObject); 
+    try {
+      let localObject = JSON.parse(dataLocalObject);
+      Object.entries(localObject).forEach(([name, value]) => {
+        form[name].value = value
+      })
+    } catch (error) {
+      
     }
-    if(object && messageInput.name === "message"){
-      messageInput.value = JSON.parse(object).message;
-    }
+    
+    // for ( prop in localObject) {
+    //   form[name.value] + localObject[prop];
+
+    // if(object && emailInput.name === "email"){
+    //     emailInput.value = JSON.parse(object).email;
+    //   }
+    //   if(object && messageInput.name === "message"){
+    //       messageInput.value = JSON.parse(object).message;
+    //     }
+
   }
+        
+  
   insrtFromLocalStorage(dataLocalObject);
-
+  
   form.addEventListener("submit", submitForm);
-
+  
   function submitForm(event){
 
     // const localObject = {
     //       elements: { email, message }
     //     } = event.currentTarget;
-        // console.log(email.value, message.value);
-        console.log(localObject);
-        let localObject = {email:' ', message:' '};
+    // console.log(email.value, message.value);
 
+
+    console.log(dataLocalObject);
+    localObject = {email:' ', message:' '};
+    
     event.preventDefault();
     event.target.reset();
     localStorage.removeItem(FEEDBACK_FORM_STATE);
-
+     
     
-
   }
-
-
+  
+  
+  
